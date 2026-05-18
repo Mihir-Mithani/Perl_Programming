7eb8332f823538e66e5b2763c192b704c12b53e7
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2026-05-18 22:52:27 +0530

    Initial commit

---
+++ Variables/arrays.pl
@@ -0,0 +1,7 @@
+#!/user/bin/perl
+@names=("Mihir","Ashutosh","Aditya");
+@ages=(21,20,22);
+
+print "$names[0] = $ages[0]\n";
+print "$names[1] = $ages[1]\n";
+print "$names[2] = $ages[2]\n";
---
+++ Variables/hash.pl
@@ -0,0 +1,6 @@
+#!/user/bin/perl
+%emp_data=('mihir',21,'ashutosh',22,'aditya',23);
+
+print " Mihir = $emp_data{'mihir'}\n";
+print " Ashutosh = $emp_data{'ashutosh'}\n";
+print " Aditya = $emp_data{'aditya'}\n";
