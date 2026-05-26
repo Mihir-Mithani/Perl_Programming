16a3626eb74d77a087c11566a249b5d09053f9af
MIHIR MITHANI <157868082+techavenger123@users.noreply.github.com>
2026-05-26 12:44:17 +0530

    String Operations

---
+++ Strings/string_operations.pl
@@ -0,0 +1,43 @@
+#!/user/bin/perl
+
+$string1="Mihir";
+$string2=" ";
+$string3="Mithani";
+
+$name=$string1.$string2.$string3;
+
+print"$name\n";
+
+#multiplication operation
+$stringvar="Mihir";
+print($stringvar*2);
+print"\n";
+
+$stringvar2="123Mihir456";
+print($stringvar2*2);
+print"\n";
+
+#increment operation
+$stringvar="Mihir";
+$stringvar++;
+print($stringvar);
+print"\n";
+
+#repetation operation
+$stringvar2=" Mihir" x 5;
+print($stringvar2);
+
+#incremetnt of z = aa
+$str="z";
+$str++;
+print("\n$str\n");
+
+#incremetnt of zz = aaa
+$str="zz";
+$str++;
+print("\n$str\n");
+
+#incremetnt of alpha numeric
+$str="1.2p34";
+$str++;
+print("\n$str\n");
---
+++ Variables/scalar.pl
@@ -0,0 +1,10 @@
+#!/user/bin/perl
+print "hello , world\n";
+
+$employee_name="Mihir";
+$employee_age=21;
+$employee_salary=100000;
+
+print "$employee_name\n";
+print "$employee_age\n";
+print "$employee_salary\n";
